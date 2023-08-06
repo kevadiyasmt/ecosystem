1e3f4c2adcf30651d35b354701919600ac6c2b70
smeet kevadiya <kevadiyasmt@gmail.com>
2023-08-06 15:00:26 +0530

    project link change

--- setup.py
+++ setup.py
@@ -10,15 +10,20 @@ setup(
     description="an ecosystem of various data transformers, structure builders, manipulators, workflows",
     long_description="an ecosystem of various data transformers, structure builders, manipulators, workflows",
     long_description_content_type="text/markdown",
-    url="https://github.com/kevadiyasmt/ecosystem",
+    url="https://github.com/kevadiyasmt/pyecosystem",
     packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+		'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     install_requires=[
-        # List any dependencies here
+        asyncio
     ],
     python_requires=">=3.7",
 )
