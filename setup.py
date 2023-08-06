bd8df6bfa0ae90a89a5d565a89f774aae692b06c
smeet kevadiya <kevadiyasmt@gmail.com>
2023-08-06 15:10:21 +0530

    setup changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyecosystem",
-    version="0.0.1",
+    version="0.0.2",
     author="Smeet Kevadiya",
     author_email="",
     description="an ecosystem of various data transformers, structure builders, manipulators, workflows",
